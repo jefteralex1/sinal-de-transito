201a92e0955b33767924face965011a7593e4077
Jefter Alexandre <103465537+jefteralex1@users.noreply.github.com>
2024-04-07 15:07:04 -0300

    add comments

--- sinal.c
+++ sinal.c
@@ -1,23 +1,38 @@
 //SINAL DE TRÂNSITO
 
-int ledvermelho = 13;
-int ledamarelo = 10;
-int ledverde = 7;
+// Definição dos pinos dos LEDs
+int ledvermelho = 13;  // Pino para o LED vermelho
+int ledamarelo = 10;   // Pino para o LED amarelo
+int ledverde = 7;      // Pino para o LED verde
 
-
+// Função setup() é executada uma vez quando o Arduino é inicializado
 void setup(){
- pinMode(ledvermelho, OUTPUT);
- pinMode(ledamarelo, OUTPUT);
- pinMode(ledverde, OUTPUT);
+  // Configura os pinos dos LEDs como saída
+  pinMode(ledvermelho, OUTPUT);  // Define o pino como saída para o LED vermelho
+  pinMode(ledamarelo, OUTPUT);   // Define o pino como saída para o LED amarelo
+  pinMode(ledverde, OUTPUT);     // Define o pino como saída para o LED verde
 }
+
+// Função loop() é executada repetidamente após a função setup()
 void loop(){
+  // Acende o LED vermelho
   digitalWrite(ledvermelho, HIGH);
+  // Aguarda 4 segundos
   delay(4000);
+  // Apaga o LED vermelho
   digitalWrite(ledvermelho, LOW);
+  
+  // Acende o LED verde
   digitalWrite(ledverde, HIGH);
+  // Aguarda 3 segundos
   delay(3000);
+  // Apaga o LED verde
   digitalWrite(ledverde, LOW);
+  
+  // Acende o LED amarelo
   digitalWrite(ledamarelo, HIGH);
+  // Aguarda 2 segundos
   delay(2000);
+  // Apaga o LED amarelo
   digitalWrite(ledamarelo, LOW);
 }
